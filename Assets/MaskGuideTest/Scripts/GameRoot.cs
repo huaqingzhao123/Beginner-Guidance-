f32dbf54d54f95fea9ea9c307ba1cd3d146ec97c
huaqingzhao123 <2296298814@qq.com>
2022-02-19 23:01:20 +0800

    第一次提交

---
+++ Assets/MaskGuideTest/Scripts/GameRoot.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace MaskTest
+{
+    public class GameRoot : MonoBehaviour, IPointerClickHandler
+    {
+        public static GameRoot Instance
+        {
+            get; set;
+        }
+        public Transform Target
+        {
+            get { return _rectMaskControl.Target; }
+        }
+        CircleMaskControl _circleMaskControl;
+        RectMaskControl _rectMaskControl;
+        Transform[] _targets;
+        private void Awake()
+        {
+            Instance = this;
+        }
+        private void Start()
+        {
+            _circleMaskControl = transform.Find("CircleMask").GetComponent<CircleMaskControl>();
+            _rectMaskControl = transform.Find("RectMask").GetComponent<RectMaskControl>();
+            //指向指定目标
+            //_circleMaskControl.SetCurTarget();
+            //此处采用矩形区域遮罩，圆形遮罩同理
+            _rectMaskControl.SetCurTarget();
+            _targets = _rectMaskControl.targets;
+            for (int i = 0; i < _targets.Length; i++)
+            {
+                if (i != _targets.Length - 1)
+                    _targets[i].GetComponent<Button>().onClick.AddListener(() =>
+                    {
+                        _rectMaskControl.CurTargetDone();
+                        _rectMaskControl.SetCurTarget();
+                    });
+                else
+                {
+                    _targets[i].GetComponent<Button>().onClick.AddListener(() =>
+                    {
+#if UNITY_EDITOR
+                        UnityEditor.EditorApplication.isPlaying = false;
+
+#else
+                         Application.Quit();
+#endif
+                    });
+                }
+            }
+        }
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            PassEvent(eventData, ExecuteEvents.pointerClickHandler);
+        }
+
+        private void PassEvent<T>(PointerEventData pointerEventData, ExecuteEvents.EventFunction<T> eventFunction)
+            where T : IEventSystemHandler
+        {
+            List<RaycastResult> results = new List<RaycastResult>();
+            EventSystem.current.RaycastAll(pointerEventData, results);
+            GameObject current = pointerEventData.pointerCurrentRaycast.gameObject;
+            for (int i = 0; i < results.Count; i++)
+            {
+                if (results[i].gameObject == Target)
+                {
+                    ExecuteEvents.Execute(results[i].gameObject, pointerEventData, eventFunction);
+                }
+            }
+        }
+    }
+
+}
+
---
+++ Assets/MaskGuideTest/Scripts/TestLocation.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestLocation : MonoBehaviour
+{
+    void Start()
+    {
+        var corners = new Vector3[4];
+        //overlay情况下返回的是屏幕坐标
+        (transform as RectTransform).GetWorldCorners(corners);
+        Debug.LogErrorFormat("左下角:{0},左上角:{1},右上角:{2},右下角:{3}",corners[0],corners[1],corners[2],corners[3]);
+        var canvas = transform.parent.GetComponent<Canvas>();
+        Debug.LogErrorFormat("转换为画布中坐标后:左下角:{0},左上角:{1},右上角:{2},右下角:{3}",WorldToCanvasPos(canvas, corners[0]), 
+            WorldToCanvasPos(canvas, corners[1]), WorldToCanvasPos(canvas, corners[2]), WorldToCanvasPos(canvas, corners[3]));
+    }
+
+    /// <summary>
+    /// 世界坐标向画布坐标转换
+    /// </summary>
+    /// <param name="canvas">画布</param>
+    /// <param name="world">世界坐标</param>
+    /// <returns>返回画布上的二维坐标</returns>
+    Vector2 WorldToCanvasPos(Canvas canvas, Vector3 world)
+    {
+        Vector2 position;
+
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(canvas.transform as RectTransform, world,null, out position);
+
+        return position;
+    }
+
+}
